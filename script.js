e9b4f68a2741e6543cde26ebae0df904fba2ac0d
Andre Maia <andrefnkmm@gmail.com>
2022-05-16 21:39:02 -0300

    finalizando script de validação de cpf

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
 console.log("Javascript carregado");
 
-function validaCPF() {
-    return false;
+function validaCPF(cpf) {
+    console.log(cpf.length);
+    if (cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0,9);
+            var digitos = cpf.substring(9);
+        
+        var soma = 0;    
+        for(var i = 10; i > 1; i--) {
+                soma += numeros.charAt (10 - i) * i;
+            }
+            console.log(soma);
+
+            var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+            //Validação do primeiro Digito
+            if(resultado != digitos.charAt(0)){
+                return false;
+            }
+
+            soma = 0;
+            numeros = cpf.substring(0,10);
+
+            for (var k = 11; k > 1; k--){
+                soma += numeros.charAt(11 - k) * k;
+            }
+
+            resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+            if(resultado != digitos.charAt(1)){
+                return false
+            }
+
+            return true;
+    }
 }
 
 function validacao(){
-    console.log("Iniciando validação de CPF...")
-
+    console.log("Iniciando validação de CPF...");
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
+ 
     var cpf = document.getElementById('cpf_digitado').value;
-    
+
     var resultadoValidacao = validaCPF(cpf);
 
     if (resultadoValidacao) {
