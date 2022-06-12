9d5f7853f1308b635ca1ed5c6b2e3a75583ab0fd
Andre Maia <andrefnkmm@gmail.com>
2022-06-12 19:00:46 -0300

    Primeiro teste para implementação das regras de cadastro de candidatura

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,14 +1,36 @@
-const http = require('http');
+// Definição de endereço / URL
+const http = require('http'); 
+const url = require('url');
+const queryString = require('query-string');
 
-const hostname = '127.0.0.1';
+const hostname = '127.0.0.1'; // localhost
 const port = 3000;
 
+// Implementação da regra de negócio
 const server = http.createServer((req, res) => {
+    
+    // Pegar a pergunta na URL
+    const params = queryString.parse(url.parse(req.url, true).search);
+
+    //Verificar a pergunta e escolher a resposta
+    let resposta;
+    if(params.cargo == 'professor-geografia'){
+        resposta = 'Sua aplicação para Prof de Geografia foi efetuada com sucesso!';
+    } 
+    else if(params.cargo == 'professor-matematica'){
+        resposta = 'Sua aplicação para Prof de Matematica foi efetuada com sucesso!';
+    }
+    else {
+        resposta = 'Sem vagas para a opção selecionada :(';
+    }
+
+    // retornar a resposta escolhida
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    res.end(resposta);
 });
 
+// execução
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
